28a3da8e475c404a2af788746d0e019b609c5920
Yash Vishnoi <165247200+yvishi@users.noreply.github.com>
2026-09-02 11:34:11 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=00000000000000000000000000000000;
+        for(int i=0;i<32;++i){
+            if(n & (1<<i)){
+                ans= ans | 1<<(31-i);
+            }
+        }
+        return ans;
+    }
+};
